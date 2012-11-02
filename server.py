3597d4bcff1ec6c672d5c6f340589ee33daad1c4
Rishi Bajekal <rishi.bajekal@gmail.com>
2012-11-02 18:42:55 -0500

    Updated titles and headers to not use fitquo

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import tornado.ioloop
 import tornado.web
 import pymongo
 from tornado.options import options, define
+from handlers.login import *
 
 define("port", default=8888, help="run on the given port", type=int)
 define("debug", default=True, help="run tornado in debug mode", type=bool)
@@ -13,11 +14,15 @@ define("debug", default=True, help="run tornado in debug mode", type=bool)
 class Application(tornado.web.Application):
     def __init__(self):
 
-        conn = pymongo.connection.Connection()
+        # conn = pymongo.connection.Connection()
         # self.db = conn['NAME OF DB']
 
         handlers = [
+            # Page Handlers
             tornado.web.URLSpec(r'/', IndexHandler),
+
+            # API Handlers
+            tornado.web.URLSpec(r'/login', FacebookLogin)
         ]
 
         current_dir = os.path.dirname(__file__)
@@ -25,9 +30,12 @@ class Application(tornado.web.Application):
         settings = dict(
             template_path=os.path.join(current_dir, 'templates'),
             static_path=os.path.join(current_dir, 'static'),
+            login_url='/login',
             debug=options.debug,
             autoescape='xhtml_escape',
-            cookie_secret='074cff43374c4c9d4e4136f884baccbe'
+            cookie_secret='074cff43374c4c9d4e4136f884baccbe',
+            facebook_api_key='417364814995960',
+            facebook_secret='271a6b543e1ad1c410dc4e6bf8ccd430',
         )
 
         super(Application, self).__init__(handlers, **settings)
