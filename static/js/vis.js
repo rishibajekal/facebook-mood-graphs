d976905ac1b5a592b82d045af4c8c5cfac1b2cfe
suharshs <suharshs@gmail.com>
2012-11-02 17:53:24 -0500

    write your changes in index.html

---
+++ static/js/vis.js
@@ -0,0 +1,19 @@
+/**
+ *  This file will hold the first visualization. This will plot time vs. sentiment.
+ */
+
+/*
+    JSON object :
+    { week : x , sentiment : y }
+*/
+
+testJSON = {
+    week1: 0.4,
+    week2: -0.2,
+    week3: -1,
+    week4: 1,
+    week5: 0.97,
+    week6: 0,
+    week7: -0.23
+};
+
