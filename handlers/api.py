7a56336875a543f2f0df50e8659f2165906c98d6
Mohd Irtefa <irtefa1@illinois.edu>
2012-11-03 03:10:46 -0500

    added lat,lng translator

--- handlers/api.py
+++ handlers/api.py
@@ -1,4 +1,5 @@
 import facebook
+from googlemaps import GoogleMaps
 from tornado.web import asynchronous
 from handlers.base import BaseHandler
 import simplejson as json
@@ -14,3 +15,9 @@ class FBStatusHandler(BaseHandler):
         feed = graph.request("", post_args={"batch": status_request})
         for status in json.loads(feed[0]["body"]):
             print status
+        person = graph.get_object("me")
+        person = json.loads(json.dumps(person))
+        location = person["location"]
+        location_name = location['name']
+        gmaps = GoogleMaps('AIzaSyCJbY1fY627epIevgIrg_XLViV5VpSOHOw')
+        lat, lng = gmaps.address_to_latlng(location_name)
