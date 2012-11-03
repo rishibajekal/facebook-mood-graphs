40bc75e659780e81dbf061747d9855dc1eab886a
Rishi Bajekal <rishi.bajekal@gmail.com>
2012-11-03 02:06:03 -0500

    Removed place from statuses

--- handlers/api.py
+++ handlers/api.py
@@ -10,7 +10,7 @@ class FBStatusHandler(BaseHandler):
     def get(self):
         user = json.loads(self.get_current_user())
         graph = facebook.GraphAPI(user["access_token"])
-        status_request = """[{"method":"GET","relative_url":"method/fql.query?query=select+message,time,place_id+from+status+where+uid=me()"}]"""
+        status_request = """[{"method":"GET","relative_url":"method/fql.query?query=select+message,time+from+status+where+uid=me()"}]"""
         feed = graph.request("", post_args={"batch": status_request})
         for status in json.loads(feed[0]["body"]):
             print status
